9d1898655abf453bde1264dc1b2ea73159a24fc6
Sanjay <sanjayrapelli172@gmail.com>
2026-03-09 14:33:33 +0530

    Modified server.js code

--- course_enroll/server.js
+++ course_enroll/server.js
@@ -28,7 +28,17 @@ const app = express();
 // };
 
 const corsOptions = {
-  origin: 'https://course-enrollment-one.vercel.app',
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      'http://localhost:3000',
+      'https://course-enrollment-one.vercel.app'
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
